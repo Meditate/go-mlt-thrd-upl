01ae56e9d7b1783a0e80b024316920fd03606d78
meditate <aroundofyou@gmail.com>
2021-04-03 21:22:30 +0200

    Read file in multiple streams sequentially

--- go-mlt-thrd-upl.go
+++ go-mlt-thrd-upl.go
@@ -3,9 +3,42 @@ package main
 import (
 	"fmt"
 	"github.com/radovskyb/watcher"
+	"io"
 	"log"
+	"math"
+	"os"
 	"time"
 )
+
+func processFile(path string) {
+	file, err := os.Open(path)
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	fileStat, err := file.Stat()
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	defer file.Close()
+
+	streamSize := math.Round(float64((fileStat.Size() / 3.0)) + 0.5)
+
+	fmt.Printf("file size: %d\n", fileStat.Size())
+
+	p := make([]byte, int(streamSize))
+
+	for i := 0; i < 3; i++ {
+		n, err := file.Read(p)
+
+		if err == io.EOF {
+			log.Fatalln(err)
+		}
+
+		fmt.Printf("%d bytes read\n", n)
+	}
+}
 
 func watcherHandler(w *watcher.Watcher) {
 	for {
@@ -13,8 +46,11 @@ func watcherHandler(w *watcher.Watcher) {
 		case event := <-w.Event:
 			if event.IsDir() && event.Op == watcher.Write {
 				continue
+			} else {
+				fmt.Println(event.Path)
+
+				processFile(event.Path)
 			}
-			fmt.Println(event)
 		case err := <-w.Error:
 			log.Fatalln(err)
 		case <-w.Closed:
